255f1df2a8efe24db3396005ee7f5e48f7ff8b80
Souleymane03 <66650099+Souleymane03@users.noreply.github.com>
2022-03-30 11:57:09 +0200

    Update and rename holberton.h to main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -11,4 +11,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
